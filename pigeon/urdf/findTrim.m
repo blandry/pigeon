421ed9fb941b3d068b079fd56c4ca6adcac76dd1
Benoit Landry <landry@mit.edu>
2014-08-31 12:26:53 -0400

    pigeon w working trim

---
+++ pigeon/urdf/findTrim.m
@@ -0,0 +1,127 @@
+function [xstar, ustar] = findTrim(p)
+
+if (nargin<1)
+  options.floating = true;
+  p = RigidBodyManipulator('pigeon.URDF', options);
+  
+  p = p.weldJoint('tail_roll');
+  p = p.weldJoint('tail_pitch');
+  p = p.weldJoint('tail_yaw');
+  p = p.weldJoint('left_hip_roll');
+  p = p.weldJoint('left_hip_pitch');
+  p = p.weldJoint('left_knee_pitch');
+  p = p.weldJoint('left_ankle_pitch');
+  p = p.weldJoint('left_thumb_pitch');
+  p = p.weldJoint('left_fingers_pitch');
+  p = p.weldJoint('right_hip_roll');
+  p = p.weldJoint('right_hip_pitch');
+  p = p.weldJoint('right_knee_pitch');
+  p = p.weldJoint('right_ankle_pitch');
+  p = p.weldJoint('right_thumb_pitch');
+  p = p.weldJoint('right_fingers_pitch');
+  p = p.compile(); 
+  
+end
+
+prog = FixedPointProgram(p,[1;3]);  % ignores xdot and zdot constraints
+
+frame = MultiCoordinateFrame({getStateFrame(p); getInputFrame(p)});
+
+lb = Point(frame);
+ub = Point(frame);
+lb.base_z = 5;  ub.base_z = 5;
+lb.base_pitch = -1.5;  ub.base_pitch = 0;
+lb.left_shoulder_roll = -1.5;  ub.left_shoulder_roll = 1.5;
+lb.left_shoulder_pitch = -1.5;  ub.left_shoulder_pitch = 1.5;
+lb.left_shoulder_yaw = -1.5;  ub.left_shoulder_yaw = 1.5;
+lb.left_elbow_yaw = -1.5;  ub.left_elbow_yaw = 1.5;
+lb.left_wrist_roll = -1.5;  ub.left_wrist_roll = 1.5;
+lb.left_wrist_pitch = -1.5;  ub.left_wrist_pitch = 1.5;
+lb.left_wrist_yaw = -1.5;  ub.left_wrist_yaw = 1.5;
+lb.right_shoulder_roll = -1.5;  ub.right_shoulder_roll = 1.5;
+lb.right_shoulder_pitch = -1.5;  ub.right_shoulder_pitch = 1.5;
+lb.right_shoulder_yaw = -1.5;  ub.right_shoulder_yaw = 1.5;
+lb.right_elbow_yaw = -1.5;  ub.right_elbow_yaw = 1.5;
+lb.right_wrist_roll = -1.5;  ub.right_wrist_roll = 1.5;
+lb.right_wrist_pitch = -1.5;  ub.right_wrist_pitch = 1.5;
+lb.right_wrist_yaw = -1.5;  ub.right_wrist_yaw = 1.5;
+lb.base_xdot = 20;  ub.base_xdot = 30;
+lb.base_zdot = -2;  ub.base_zdot = 0;
+lb.left_shoulder_roll_servo = -Inf;  ub.left_shoulder_roll_servo = Inf;
+lb.left_shoulder_pitch_servo = -Inf;  ub.left_shoulder_pitch_servo = Inf;
+lb.left_shoulder_yaw_servo = -Inf;  ub.left_shoulder_yaw_servo = Inf;
+lb.left_elbow_yaw_servo = -Inf;  ub.left_elbow_yaw_servo = Inf;
+lb.left_wrist_roll_servo = -Inf;  ub.left_wrist_roll_servo = Inf;
+lb.left_wrist_pitch_servo = -Inf;  ub.left_wrist_pitch_servo = Inf;
+lb.left_wrist_yaw_servo = -Inf;  ub.left_wrist_yaw_servo = Inf;
+lb.right_shoulder_roll_servo = -Inf;  ub.right_shoulder_roll_servo = Inf;
+lb.right_shoulder_pitch_servo = -Inf;  ub.right_shoulder_pitch_servo = Inf;
+lb.right_shoulder_yaw_servo = -Inf;  ub.right_shoulder_yaw_servo = Inf;
+lb.right_elbow_yaw_servo = -Inf;  ub.right_elbow_yaw_servo = Inf;
+lb.right_wrist_roll_servo = -Inf;  ub.right_wrist_roll_servo = Inf;
+lb.right_wrist_pitch_servo = -Inf;  ub.right_wrist_pitch_servo = Inf;
+lb.right_wrist_yaw_servo = -Inf;  ub.right_wrist_yaw_servo = Inf;
+
+A = [];
+
+    function addSymmetricConstraint(jointA,jointB)
+        pt = Point(frame);
+        pt.(jointA) = 1;
+        pt.(jointB) = -1;
+        A = [A; double(pt)'];
+    end
+
+    function addAntiSymmetricConstraint(jointA,jointB)
+        pt = Point(frame);
+        pt.(jointA) = 1;
+        pt.(jointB) = 1;
+        A = [A; double(pt)'];
+    end
+
+
+addSymmetricConstraint('left_shoulder_roll','right_shoulder_roll');
+addSymmetricConstraint('left_shoulder_pitch','right_shoulder_pitch');
+addSymmetricConstraint('left_shoulder_yaw','right_shoulder_yaw');
+addSymmetricConstraint('left_elbow_yaw','right_elbow_yaw');
+addSymmetricConstraint('left_wrist_roll','right_wrist_roll');
+addSymmetricConstraint('left_wrist_pitch','right_wrist_pitch');
+addSymmetricConstraint('left_wrist_yaw','right_wrist_yaw');
+
+
+xdot_ind = findCoordinateIndex(getStateFrame(p),'base_xdot');
+zdot_ind = findCoordinateIndex(getStateFrame(p),'base_zdot');
+ 
+prog = addConstraint(prog,BoundingBoxConstraint(double(lb),double(ub)));
+prog = addConstraint(prog,FunctionHandleObjective(prog.num_vars,@(x)cost(x,xdot_ind,zdot_ind)));
+prog = addConstraint(prog,LinearConstraint(zeros(7,1),zeros(7,1),A));
+
+x0 = Point(getStateFrame(p));
+x0.base_xdot = 20;
+x0.base_z = 5;
+num_inputs = getNumInputs(p);
+num_states = getNumStates(p);
+u0 = zeros(num_inputs,1);
+
+w = prog.solve([double(x0);u0]); 
+xstar = w(1:num_states);
+ustar = w(num_states+1:end);
+
+if (nargout<1)
+  utraj = ConstantTrajectory(ustar);
+  utraj = setOutputFrame(utraj,getInputFrame(p));
+  sys = cascade(utraj,p);
+  xtraj = simulate(sys,[0 .1],xstar);
+  v = p.constructVisualizer(); 
+  v.playback(xtraj, struct('slider',true));
+end
+
+end
+
+function [c,dc] = cost(x,xdot_ind,zdot_ind)
+    xdot = x(xdot_ind);
+    zdot = x(zdot_ind);
+    c = zdot/xdot;
+    dc = 0*x';
+    dc(xdot_ind) = -zdot/xdot^2;
+    dc(zdot_ind) = 1/xdot;
+end
---
+++ pigeon/urdf/runDircolFlapping.m
@@ -0,0 +1,75 @@
+function [utraj, xtraj] = runDircolFlapping
+
+options.floating = true;
+p = RigidBodyManipulator('pigeon_17.URDF', options);
+
+N = 11;
+minimum_duration = .1;
+maximum_duration = 3;
+prog = DircolTrajectoryOptimization(p,N,[minimum_duration maximum_duration]); 
+
+prog = prog.setSolverOptions('snopt','superbasicslimit',2000);
+% prog = prog.setCheckGrad(true);
+prog = prog.addDisplayFunction(@(x)displayfun(x,N,p.getNumStates(),p.getNumInputs()));
+
+% Ideally this is what we want
+tol = zeros(p.getNumStates(),1);
+tol(1) = Inf; % ignore horizontal position
+% but we should make this work first
+% this only cares about the altitude
+ tol = Inf*ones(p.getNumStates(),1);
+ tol(3) = .1; % only require the bird to keep its altitude
+prog = prog.addStateConstraint(PeriodicConstraint(tol),{[1 N]});
+
+prog = prog.addRunningCost(@cost);
+prog = prog.addFinalCost(@finalcost);
+
+display('Finding trim conditions for initial guess...')
+[xstar,ustar] = findTrim(p);
+tf0 = 1;  % initial guess at duration 
+traj_init.x = PPTrajectory(foh([0,tf0],[double(xstar),...
+  double(xstar)+tf0*[double(xstar(23:44));zeros(22,1)]]));
+traj_init.u = ConstantTrajectory(ustar);
+
+display('Starting trajectory optimization...')
+tic
+[xtraj,utraj,z,F,info] = prog.solveTraj(tf0,traj_init);
+toc
+
+if (nargout<1)
+  v = constructVisualizer(p);
+  v.playback_speed = .2;
+  v.playback(xtraj,struct('slider',true));
+end
+
+end
+
+function [g,dg] = cost(t,x,u)
+  Q = zeros(44,44);
+  Q(4:6,4:6) = 10000*eye(3); % penalize roll pitch and yaw
+  Q(26:28,26:28) = 1000*eye(3); % penalize rolldot, pitchdot, yawdot
+  R = eye(16);
+  g = x'*Q*x + u'*R*u;
+  if (nargout>1)
+    dg = [0,2*x'*Q,2*u'*R];
+  end
+end
+
+function [h,dh] = finalcost(t,x)
+  h = t;
+  if (nargout>1)
+    dh = [1,zeros(1,size(x,1))];
+  end
+end
+
+function displayfun(X,num_t,num_x,num_u)
+  h = X(1:num_t-1);
+  x = X(num_t-1+[1:num_x*num_t]);
+  u = X(num_t-1+num_x*num_t+[1:num_u*num_t]);
+  x = reshape(x,num_x,num_t);
+  u = reshape(u,num_u,num_t);
+  figure(5);
+  plot3(x(1,:),x(2,:),x(3,:));
+  % plot(x(1,:),x(3,:));
+  % plot([0,cumsum(h)'],x(1,:));
+end
